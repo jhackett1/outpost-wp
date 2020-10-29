08960d971489dfe22db8137dd4b3c19468015ba8
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-29 17:16:26 +0000

    proper serializers for things

--- api/serializers.php
+++ api/serializers.php
@@ -7,24 +7,73 @@ function base_serializer($post){
     );
 }
 
+function location_serializer($post){
+    return array(
+        "id" => $post->ID,
+        "name" => $post->post_title,
+        "latitude" => "",
+        "longitude" => "",
+        "physical_address" => array(
+            "address_1" => "",
+            "city" => "",
+            "country" => "",
+            "postal_code" => "",
+            "state_province" => ""
+        )
+    );
+}
+
+function contact_serializer($post){
+    return array(
+        "id" => $post->ID,
+        "name" => $post->post_title,
+        "title" => get_field("role", $post),
+        "phone" => get_field("phone", $post),
+        "email" => get_field("email", $post)
+    );
+}
+
+function schedule_serializer($post){
+    return array(
+        "id" => $post->ID,
+        "opens_at" => get_field("opens_at", $post),
+        "closes_at" => get_field("closes_at", $post),
+    );
+}
+
+function cost_option_serializer($post){
+    return array(
+        "id" => $post->ID,
+        "option" => $post->post_title,
+        "amount" => get_field("amount", $post),
+        "amount_description" => get_field("amount_description", $post)
+    );
+}
+
 function service_serializer($post){
     return array(
+        
         "id" => $post->ID,
         "name" => $post->post_title,
         "description" => $post->post_content,
         "url" => get_field("url", $post),
-        "organisation" => base_serializer(get_field("organisation", $post)),
+
+        "organization" => base_serializer(get_field("organisation", $post)),
+
         "locations" => array_map(function($location){ 
-            return base_serializer($location); 
+            return location_serializer($location); 
         }, posts_belonging_to_service("location", $post->ID)),
+
         "contacts" => array_map(function($contact){ 
-            return base_serializer($contact); 
+            return contact_serializer($contact); 
         }, posts_belonging_to_service("contact", $post->ID)),
+
         "cost_options" => array_map(function($cost_option){ 
-            return base_serializer($cost_option); 
+            return cost_option_serializer($cost_option); 
         }, posts_belonging_to_service("cost_option", $post->ID)),
-        "schedules" => array_map(function($schedules){ 
-            return base_serializer($schedules); 
+
+        "regular_schedules" => array_map(function($schedules){ 
+            return schedule_serializer($schedules); 
         }, posts_belonging_to_service("schedule", $post->ID))
     );
 }
