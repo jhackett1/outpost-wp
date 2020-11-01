f9a12804b5d822fb8137bda11ca8957fdb096db2
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-11-01 15:29:52 +0000

    fix typo

--- inc/geo-search.php
+++ inc/geo-search.php
@@ -12,7 +12,7 @@ function handle_geo_select($select_clause, $query) {
             wp_posts.*,
             wp_geo.latitude AS latitude,
             wp_geo.longitude AS longitude,
-            ( $radius * acos(
+            ( $earth_radius * acos(
                 cos( radians( $lat ) )
                 * cos( radians( latitude ) )
                 * cos( radians( longitude ) - radians( $lng ) )
@@ -42,21 +42,18 @@ add_filter('posts_join_paged','handle_geo_joins', 10, 2);
 
 // function handle_geo_where($where_clause, $query){
 //     if($query->get("geo_query") && $query->get("geo_query")["radius"]){
-
 //         $earth_radius = 3959;
-
 //         $lat = $query->get("geo_query")["latitude"];
 //         $lng = $query->get("geo_query")["longitude"];
 //         $radius = $query->get("geo_query")["radius"];
-
 //         $where_clause .= "AND 
-//                     ( $radius * acos(
-//                 cos( radians( $lat ) )
-//                 * cos( radians( latitude ) )
-//                 * cos( radians( longitude ) - radians( $lng ) )
-//                 + sin( radians( $lat ) )
-//                 * sin( radians( latitude ) )
-//                 ) ) < $radius
+//             ( $earth_radius * acos(
+//             cos( radians( $lat ) )
+//             * cos( radians( latitude ) )
+//             * cos( radians( longitude ) - radians( $lng ) )
+//             + sin( radians( $lat ) )
+//             * sin( radians( latitude ) )
+//             ) ) < $radius
 //         ";
 //     }
 // 	return $where_clause;	
